df62f677d45226fc1ccb5cd7764b768bc94eb82d
Jan Jakubik <janekjak@gmail.com>
2020-07-08 21:19:03 +0200

    add cryptography

--- src/broker/broker.ts
+++ src/broker/broker.ts
@@ -1,9 +1,11 @@
-import aedes, {Aedes, Client} from 'aedes';
+import aedes, {Aedes, AedesPublishPacket, Client} from 'aedes';
 import net from 'net';
 import {SecureClient} from './client';
+import {Buffer} from 'buffer';
+import {Ecies} from 'eccrypto';
 
 export class Broker {
-    private aedes: Aedes = aedes();
+    private aedes: Aedes = aedes({authorizeForward: this.forwardHandler.bind(this)});
     private server;
     private clients: Array<SecureClient> = [];
 
@@ -11,10 +13,7 @@ export class Broker {
         this.server = net.createServer(this.aedes.handle);
         this.server.listen(port, () => console.log(`S-MQTT server starts on port: ${port}`));
 
-        console.log(this.aedes);
-
         this.aedes.on('client', (client: Client) => {
-            console.log('Dołączono klienta: ' + client.id);
             this.clients.push(new SecureClient(client));
             const cmd: 'publish' = 'publish';
             const qos: 0 | 1 | 2 = 0;
@@ -28,5 +27,44 @@ export class Broker {
             };
             client.publish(packet, () => console.log(`Przywitano ${client.id}`));
         });
+
+        this.aedes.on('subscribe', (sub, client) => {
+            for (const t of this.clients) {
+                if (t.baseClient === client) {
+                    t.subscription = sub;
+                }
+            }
+        });
+    }
+
+
+    forwardHandler(client: Client, data: AedesPublishPacket) {
+        console.log('tp1 ' + client + ' ' + data.payload);
+        const newData = {...data};
+
+        const secureClient = this.findUser(client);
+        secureClient.encryptOutput(data.payload as Buffer).then(datae => {
+
+            const ecies: Ecies = datae;
+            newData.payload = JSON.stringify(ecies);
+            const json: any = JSON.parse(JSON.stringify(ecies));
+
+            json.iv = Buffer.from(json.iv);
+            json.ephemPublicKey = Buffer.from(json.ephemPublicKey);
+            json.ciphertext = Buffer.from(json.ciphertext);
+            json.mac = Buffer.from(json.mac);
+
+            secureClient.decryptInput(json).then(datad => console.log('Po odczytaniu: ' + datad.toString()));
+        });
+        return newData;
+    }
+
+    findUser(client: Client): SecureClient {
+        for (const c of this.clients) {
+            if (c.baseClient === client) {
+                return c;
+            }
+        }
+        return null;
     }
 }
--- src/broker/client.ts
+++ src/broker/client.ts
@@ -1,15 +1,30 @@
-import {Client} from 'aedes';
+import {Client, Subscription} from 'aedes';
+import eccrypto, {Ecies} from 'eccrypto';
+import {Buffer} from 'buffer';
 
 export class SecureClient {
-    inputPrivateKey: string;
-    inputPublicKey: string;
-    outputPrivateKey: string;
-    outputPublicKey: string;
+    inputPrivateKey: Buffer;
+    inputPublicKey: Buffer;
+    outputPrivateKey: Buffer;
+    outputPublicKey: Buffer;
+    subscription: Array<Subscription> = [];
 
-    constructor(private baseClient: Client) {
-        this.inputPrivateKey = 'xxx';
-        this.inputPublicKey = 'xxx';
-        this.outputPrivateKey = 'xxx';
-        this.outputPublicKey = 'xxx';
+    constructor(public baseClient: Client) {
+        this.generateKey();
+    }
+
+    async decryptInput(data: Ecies): Promise<Buffer> {
+        return await eccrypto.decrypt(this.outputPrivateKey, data);
+    }
+
+    async encryptOutput(data: Buffer): Promise<Ecies> {
+        return await eccrypto.encrypt(this.outputPublicKey, data);
+    }
+
+    private generateKey() {
+        this.inputPrivateKey = eccrypto.generatePrivate();
+        this.inputPublicKey = eccrypto.getPublic(this.inputPrivateKey);
+        this.outputPrivateKey = eccrypto.generatePrivate();
+        this.outputPublicKey = eccrypto.getPublic(this.outputPrivateKey);
     }
 }
